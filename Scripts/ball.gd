5de70cac197620c936b2761016187d3a50ef742a
MystrMeat <bradenballachay8888@gmail.com>
2025-03-29 10:32:46 -0600

    Game, Menu, Global, Ball
    Added a gamer screen, a menu screen, a ball with physics, and a global scene

---
+++ Scripts/ball.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+var d := 0.0
+var radius := 250
+var speed := 2
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	d += delta
+	
+	position = Vector2(
+		sin(d * speed) * radius,
+		cos(d * speed) * radius
+	)
+	
