d3cd8e0faf6312282a05bbbf547541d3697df745
MystrMeat <bradenballachay8888@gmail.com>
2025-03-29 12:17:15 -0600

    Edited global values

--- Scripts/global.gd
+++ Scripts/global.gd
@@ -6,6 +6,11 @@ extends Control
 static var player_mass = 0
 static var player_money = 10000
 
+const comet_token := 10
+const moon_token := 50
+const planet_token := 100
+const star_token := 1000
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	player_mass_text.text = str(player_mass)
--- Scripts/shop.gd
+++ Scripts/shop.gd
@@ -3,10 +3,10 @@ extends Control
 @onready var timer: Timer = $Timer
 @onready var global: Control = $Global
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
