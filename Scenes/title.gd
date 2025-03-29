ba343c76400b5243533b6f030ed8f880572c9d7d
Victoria Lee <vi3@ualberta.ca>
2025-03-28 20:34:21 -0600

    created title screen, imported more assets

---
+++ Scenes/title.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+@onready var bg_cloud = preload("res://Assets/title_screen/Scrolling_Clouds_Background.png")
+@onready var fg_cloud = preload("res://Assets/title_screen/Scrolling_Clouds_Foreground.png")
+
+func _ready():
+	
+	pass
+	
+func _physics_process(delta):
+	pass
