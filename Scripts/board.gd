36030c0e9e14217047b837cac16de213320c2886
MystrMeat <bradenballachay8888@gmail.com>
2025-03-30 13:04:40 -0600

    Added split and line options to board

--- Scripts/board.gd
+++ Scripts/board.gd
@@ -1,5 +1,21 @@
 extends Node2D
 
+# bet options for outside bets
+var bet_red = false
+var bet_black = false
+var bet_odd = false
+var bet_even = false
+var bet_low = false
+var bet_high = false
+var bet_dozen = false
+
+#bet options for inside bets
+var bet_line = false
+var bet_corner = false
+var bet_street = false
+var bet_split = false
+var bet_number = false
+var bet_zero = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
--- Scripts/roulette_wheel.gd
+++ Scripts/roulette_wheel.gd
@@ -4,6 +4,7 @@ var rotation_speed
 var friction = 0.99
 var applyFriction = false
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
