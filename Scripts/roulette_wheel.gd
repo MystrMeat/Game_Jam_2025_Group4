2caedaff9daab3d09fbc2ea40ba2e8e80995669c
MystrMeat <bradenballachay8888@gmail.com>
2025-03-29 20:29:31 -0600

    Added spin physics to wheel

--- Scripts/roulette_wheel.gd
+++ Scripts/roulette_wheel.gd
@@ -1,9 +1,24 @@
 extends Node2D
 
+var rotation_speed
+var friction = 0.99
+var applyFriction = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	rotation_degrees += 1
+	if applyFriction:
+		if rotation_speed != 0:
+			var current_angle = self.rotation
+			current_angle += rotation_speed * delta
+			rotation_speed *= friction
+			if abs(rotation_speed) < 0.01:
+				rotation_speed = 0
+				applyFriction = false
+			self.rotation = current_angle
+	else:
+		rotation_degrees += 1
+					
